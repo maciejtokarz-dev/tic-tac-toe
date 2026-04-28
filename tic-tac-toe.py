00acd56a767c26f71d0e0c5bbec319f011d236c2
Macster <maciektbb@gmail.com>
2026-04-28 17:37:18 +0200

    added draw method

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -2,39 +2,40 @@ class Board:
     
     def __init__(self):
         self.listOfSquares = ["NULL"] * 9
-        i = 0
     
     def showBoard(self):
+        print("-----")
         for i, square in enumerate(self.listOfSquares):
-
-            print(square, end=" ")
-
-            if (i+1) % 3 == 0:
+            print(square, end="")
+            if(i+1) % 3 == 0:
                 print()
-
-        return self.listOfSquares
+        print("-----")
 
     def fillSquare(self, square, type):
-        self.square = square
-        self.type = type
-        
-        if type == 'X':
-            if self.listOfSquares[square] == 'NULL':
-                self.listOfSquares[square] = 'X'
-            else:
-                print("Square have to be empty! ")
-        elif type == 'O':
-            if self.listOfSquares[square] == 'NULL':
-                self.listOfSquares[square] = 'O'
-            else:
-                print("Square have to be empty! ")
+         
+        if not (0 <= square <= 8):
+            print(f"Indeks {square} nie poprawny! Wybierz indeks z przedzialu: (0-8)")
+            return
 
+        if self.listOfSquares[square] == "NULL":
+            self.listOfSquares[square] = type
+        else:
+            print(f"Square {square} have to be empty! Choose another square")
 
+    def draw(self):
+        if "NULL" not in self.listOfSquares:
+            print(f"It's a draw ladies and gentlemens! Now it's time for a rematch!")
+            return True
+        return False
     
 g1 = Board()
-g1.fillSquare(0, 'X')
-g1.fillSquare(0, 'X')
+
+for i in range(9):
+    g1.fillSquare(i, 'X')
+
+
 g1.showBoard()
+g1.draw()
 
 
 
