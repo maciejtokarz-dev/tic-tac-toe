bbfde762c33641741f101cf2bf1fa62ca2da8bf9
Macster <maciektbb@gmail.com>
2026-04-29 20:00:12 +0200

    added class Player

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -11,14 +11,14 @@ class Board:
                 print()
         print("-----")
 
-    def fillSquare(self, square, type):
+    def fillSquare(self, square, symbol):
          
         if not (0 <= square <= 8):
             print(f"Indeks {square} nie poprawny! Wybierz indeks z przedzialu: (0-8)")
             return
 
         if self.listOfSquares[square] == "NULL":
-            self.listOfSquares[square] = type
+            self.listOfSquares[square] = symbol
         else:
             print(f"Square {square} have to be empty! Choose another square")
 
@@ -28,11 +28,22 @@ class Board:
             return True
         return False
     
+class Player:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+
+    def move(self):
+        square = input("Type a number of empty square: ")
+        self.fillSquare(square, self.symbol)
+        self.showBoard()
+
 g1 = Board()
 
-for i in range(9):
-    g1.fillSquare(i, 'X')
-
+#for i in range(9):
+#    g1.fillSquare(i, 'X')
+p1 = Player("maciek", "X")
+p1.move()
 
 g1.showBoard()
 g1.draw()
