855087f63275a95d9fb3dd34159fd93d4f66b986
Macster <maciektbb@gmail.com>
2026-04-30 19:50:04 +0200

    corrected returnings in methods and added class game

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,7 +1,7 @@
 class Board:
     
     def __init__(self):
-        self.listOfSquares = ["NULL"] * 9
+        self.listOfSquares = ["NULL  "] * 9
     
     def showBoard(self):
         print("-----")
@@ -15,12 +15,14 @@ class Board:
          
         if not (0 <= square <= 8):
             print(f"Indeks {square} nie poprawny! Wybierz indeks z przedzialu: (0-8)")
-            return
+            return False
 
-        if self.listOfSquares[square] == "NULL":
-            self.listOfSquares[square] = symbol
+        if self.listOfSquares[square] == "NULL  ":
+            self.listOfSquares[square] = symbol + '  '
+            return True
         else:
             print(f"Square {square} have to be empty! Choose another square")
+            return False
 
     def draw(self):
         if "NULL" not in self.listOfSquares:
@@ -36,19 +38,37 @@ class Player:
     def move(self, board):
         try:
             square = int(input("Type a number of square: "))
-            board.fillSquare(square, self.symbol)
-            board.showBoard()
+            return board.fillSquare(square, self.symbol)
         except ValueError:
             print("You have to type a number!")
+            return False
 
-g1 = Board()
+class Game:
+    def __init__(self, board, player1, player2, turn):
+        self.board = board
+        self.player1 = player1
+        self.player2 = player2
+        self.turn = turn
+        
+    def changeTurn(self):
+        if self.turn == p1:
+            self.turn = p2
+        elif self.turn == p2:
+                self.turn = p1
+
+
+
+b1 = Board()
 
 #for i in range(9):
 #    g1.fillSquare(i, 'X')
 p1 = Player("Maciek", "X")
-p1.move(g1)
+p2 = Player("Bartosz", "Y")
+g1 = Game(b1, p1, p2, p1)
+p1.move(b1)
+g1.changeTurn()
+p2.move(b1)
 
-g1.showBoard()
 
 
 
